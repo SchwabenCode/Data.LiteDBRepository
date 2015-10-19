bc2d4ed5ac06ebeb3ffffeaa4b151006a9859f92
Benjamin Abt <mail@benjamin-abt.com>
2015-10-19 17:35:41 +0200

    # initial commit

---
+++ SchwabenCode.Data.LiteDBRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SchwabenCode.Data.LiteDBRepository")]
+[assembly: AssemblyDescription( "LiteDBRepository by SchwabenCode" )]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany( "Benjamin Abt (SchwabenCode.com)" )]
+[assembly: AssemblyProduct("SchwabenCode.Data.LiteDBRepository")]
+[assembly: AssemblyCopyright( "Copyright Benjamin Abt (SchwabenCode.com) 2015" )]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cee89d9a-dd1c-4280-a7b1-b27186570ade")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion( "0.0.1-alpha" )]
+[assembly: AssemblyFileVersion("0.0.0.1")]
