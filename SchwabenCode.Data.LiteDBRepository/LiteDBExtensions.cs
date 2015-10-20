33ed5a94013875512d1730a9baaa6947ab0b3cdc
Benjamin Abt <mail@benjamin-abt.com>
2015-10-20 02:09:05 +0200

    + extensions class

---
+++ SchwabenCode.Data.LiteDBRepository/LiteDBExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LiteDB;
+using System.IO;
+
+namespace SchwabenCode.Data.LiteDBRepository
+{
+    public static class LiteDBExtensions
+    {
+        public static TFile ToStorageFile<TFile>( this LiteFileInfo fileInfo ) where TFile : class, IStorageFile, new()
+        {
+            TFile file = new TFile
+            {
+                Id = fileInfo.Id
+            };
+
+            // Read
+            using( LiteFileStream fs = fileInfo.OpenRead() )
+            using( MemoryStream ms = new MemoryStream() )
+            {
+                fs.CopyTo( ms );
+
+                file.Content = ms.ToArray();
+            }
+
+            return file;
+        }
+    }
+}
